177b7eb7f7a0ab43f0a055716c87ae7e0a0a821a
Jungho Son <js3692@users.noreply.github.com>
2016-02-10 22:34:22 -0500

    Model tests added

---
+++ tests/server/models/inbox-test.js
@@ -0,0 +1,31 @@
+var dbURI = 'mongodb://localhost:27017/pomodoroTest';
+var clearDB = require('mocha-mongoose')(dbURI);
+
+var mongoose = require('mongoose');
+
+var expect = require('chai').expect;
+
+require('../../../server/db/models');
+
+var Inbox = mongoose.model('Inbox');
+
+describe('Inbox model', function () {
+
+  beforeEach('Establish DB connection', function (done) {
+    if (mongoose.connection.db) return done();
+    mongoose.connect(dbURI, done);
+  });
+
+  afterEach('Clear test database', function (done) {
+    clearDB(done);
+  });
+
+  it('should exist', function () {
+    expect(Inbox).to.be.a('function');
+  });
+
+  describe('', function () {
+
+  });
+
+});
