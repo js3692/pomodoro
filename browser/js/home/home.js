9eb81b17b5ef4ffb51fbf1dc39b71ffdb0b43bc7
Jungho Son <js3692@users.noreply.github.com>
2016-02-12 17:41:18 -0500

    Almost completed home state

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,6 +1,24 @@
-app.config(function ($stateProvider) {
-  $stateProvider.state('home', {
-    url: '/',
-    templateUrl: 'js/home/home.html'
-  });
-});
+app.config(['$stateProvider', function ($stateProvider) {
+	$stateProvider.state('home', {
+		url: '/',
+		templateUrl: 'js/home/home.html',
+		controller: function (AuthService, $scope, $state) {
+			$scope.loading = false;
+			$scope.error = null;
+
+			$scope.login = function (credentials) {
+				if($scope.error) $scope.error = null;
+				$scope.loading = true;
+				AuthService.login(credentials)
+					.then(function () {
+						$scope.loading = false;
+						$state.go('dashboard');
+					})
+					.catch(function (err) {
+						$scope.loading = false;
+						$scope.error = err.message;
+					});
+			}
+		}
+	});
+}]);
