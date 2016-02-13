17f76157ec3bf062627a4b2a025029d5faa79f65
Jungho Son <js3692@users.noreply.github.com>
2016-02-13 16:30:05 -0500

    Cleanup

--- browser/js/profile/profile.js
+++
@@ -1,9 +0,0 @@
-app.config(['$stateProvider', function ($stateProvider) {
-  $stateProvider.state('profile', {
-    url: '/profile',
-    templateUrl: 'js/profile/profile.html',
-  	data: {
-    	authenticate: true
-  	}
-  });
-}]);
